822f5acc74fb52f23b58c393ffbd26e379f4376a
Seth Ricks <131173064+Sethricks340@users.noreply.github.com>
2025-09-29 11:53:42 -0600

    First few labs
    Added initial files for first couple of labs.

---
+++ adder8/adder8.srcs/sources_1/new/adder_structural.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/29/2025 10:23:37 AM
+// Design Name: 
+// Module Name: adder_structural
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module adder_structural(
+    input [8:0] SW,
+    output [8:0] LED
+    );
+endmodule
