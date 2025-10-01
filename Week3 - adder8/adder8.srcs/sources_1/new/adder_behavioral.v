db99eb0c076a2d02249f535fcebdfee62fa878a6
Seth Ricks <131173064+Sethricks340@users.noreply.github.com>
2025-10-01 11:13:53 -0600

    Added comments
    Added comments

--- Week3 - adder8/adder8.srcs/sources_1/new/adder_behavioral.v
+++ Week3 - adder8/adder8.srcs/sources_1/new/adder_behavioral.v
@@ -18,25 +18,32 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-// Creating Module
 // Input 9 switches
 // Output 9 LEDs
+// --- module for an 8-bit adder, in behavioral style verilog. --- // 
 module addern(input [15:0] sw, output [8:0] led);
+    // Changeable parameter set at 8
     parameter n = 8;
     wire [n:0] C;
     
     genvar i;
+    // The first carryin will be a zero, since it is the first addition
     assign C[0] = 1'b0; 
 
+    // generate statement needed when instantiating modules
+    // loops through n number of fulladd stages
     generate 
         for(i=0;i<=n-1;i=i+1)
-           begin:whatever
+           begin:adderbit
            fulladd stage(.Carryin(C[i]), .x(sw[i]), .y(sw[i+8]), .Sum(led[i]), .Carrout(C[i+1]));
            end
     endgenerate
 
 endmodule
 
+// --- Adder using behavioral verilog. --- //
+// Inputs: Carrin, x (bit one), and y (bit two)
+// Outputs: Sum and Carryout
 module fulladd(Carryin, x, y, Sum, Carryout);
     input Carryin, x, y;
     output Sum, Carryout;
--- Week3 - adder8/adder8.srcs/sources_1/new/adder_structural.v
+++ Week3 - adder8/adder8.srcs/sources_1/new/adder_structural.v
@@ -18,12 +18,13 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-// Creating Module
 // Input 9 switches
 // Output 9 LEDs
+// --- module for an 8-bit adder, in structural style verilog. --- // 
 module adder8(input [15:0] sw, output [8:0] led);
     wire [6:0] w;
-
+    // Declare each fulladd module separtely, and pass carrout outputs into the carrin inputs
+    // This resembles a ripple carry-in adder
     fulladd FA0 (.Carryin(1'b0), .x(sw[0]), .y(sw[8]),  .Sum(led[0]), .Carryout(w[0]));
     fulladd FA1 (.Carryin(w[0]), .x(sw[1]), .y(sw[9]), .Sum(led[1]), .Carryout(w[1]));
     fulladd FA2 (.Carryin(w[1]), .x(sw[2]), .y(sw[10]), .Sum(led[2]), .Carryout(w[2]));
@@ -32,10 +33,11 @@ module adder8(input [15:0] sw, output [8:0] led);
     fulladd FA5 (.Carryin(w[4]), .x(sw[5]), .y(sw[13]), .Sum(led[5]), .Carryout(w[5]));
     fulladd FA6 (.Carryin(w[5]), .x(sw[6]), .y(sw[14]), .Sum(led[6]), .Carryout(w[6]));
     fulladd FA7 (.Carryin(w[6]), .x(sw[7]), .y(sw[15]), .Sum(led[7]), .Carryout(led[8]));
-
 endmodule
 
-
+// --- Adder using structural verilog. --- //
+// Inputs: Carrin, x (bit one), and y (bit two)
+// Outputs: Sum and Carryout
 module fulladd(Carryin, x, y, Sum, Carryout);
     input Carryin, x, y;
     output Sum, Carryout;
@@ -44,7 +46,5 @@ module fulladd(Carryin, x, y, Sum, Carryout);
     xor (Sum, w1, Carryin);
     and (w2, x, y);
     and (w3, w1, Carryin);
-    or  (Carryout, w2, w3);
-    
-    
+    or  (Carryout, w2, w3);   
 endmodule
