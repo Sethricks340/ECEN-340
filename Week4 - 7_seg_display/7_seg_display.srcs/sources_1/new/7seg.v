1474bb966fa70be8d77a234aadb76dcb81f1cb16
Seth Ricks <131173064+Sethricks340@users.noreply.github.com>
2025-10-08 10:51:25 -0600

    Lab 4 Part 2
    Lab 4 Part 2

--- Week4 - 7_seg_display/7_seg_display.srcs/sources_1/new/7seg.v
+++ Week4 - 7_seg_display/7_seg_display.srcs/sources_1/new/7seg.v
@@ -19,11 +19,14 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+// seven segment display on FPGA board
 module seven_seg(output reg [6:0] seg, input [3:0] sw);
     
+    // sensitivity list includes everything
     always@(*)
     begin 
+        // For each switch combination, possibilities from 0-9
+        // seg is the output segments on the 7 digit display
         if (sw == 4'b0000) seg = 7'b1000000;
         else if (sw == 4'b0001) seg = 7'b1111001;
         else if (sw == 4'b0010) seg = 7'b0100100;
